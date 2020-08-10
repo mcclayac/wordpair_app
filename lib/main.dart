2781834cfd4468180c3ced0619f812e31219fd12
mcclayac <anthony.mcclay@me.com>
2020-08-10 01:02:29 -0400

    Simple LIst

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,57 @@ void main()  {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
+
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.purple),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('WordPair Generator')),
-            body: Center(child: Text(wordPair.asPascalCase))
-          ),
+        home: RandomWords()
         );
   }
 }
+
+class RandomWords extends StatefulWidget{
+  @override
+  RandomWordsState createState() => RandomWordsState();
+
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _randomRordPairs = <WordPair>[];
+  
+
+  Widget _build_list() {
+    return ListView(
+      padding: const EdgeInsets.all(16.0),
+      children: <Widget>[
+        Container(
+          height: 50,
+          color: Colors.amber[600],
+          child: const Center(child: Text('Entry A')),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[500],
+          child: const Center(child: Text('Entry B')),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[100],
+          child: const Center(child: Text('Entry C')),
+        ),
+      ],
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WordPair Genator 2'),
+      ),
+      body: _build_list(),
+    );
+  }
+}
+
 
 // class MyApp extends StatelessWidget {
 

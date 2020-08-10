4c5abf287f859c9d39cf530c6c07bd443fa1f65a
mcclayac <anthony.mcclay@gmail.com>
2020-08-09 23:18:33 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+
+void main()  {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('These McClay'));
+  }
+}
+
+
+
+// class MyApp extends StatelessWidget {
+
+//   @override 
+//   Widget build(BuildContext  context) {
+//     return MaterialApp() {
+//       home: Text("These Nutz");
+
+//     };
+//   }
+// }
+
+
+
+
